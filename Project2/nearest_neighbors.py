9eb3917c6b2bc21e43a4b79d36d05d7afc588aba
Pedram <psafaei@nevada.unr.edu>
2019-10-09 09:03:06 -0700

    Done with KNN

--- Project2/nearest_neighbors.py
+++ Project2/nearest_neighbors.py
@@ -1,12 +1,6 @@
 import numpy as np
 import math # just using the pow for calculating the norm
 
-X = np.array ([[1, 1], [2, 1], [0, 10], [10, 10], [5, 5], [3, 10], [9, 4], [6, 2], [2, 2], [8, 7]])
-Y = np.array( [[1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [-1]])
-
-XT = np.array([[1, 5], [2, 6], [2, 7], [3, 7], [3, 8], [4, 8], [5, 1], [5, 9], [6, 2], [7, 2], [7, 3], [8, 3], [8, 4], [9, 5]])
-YT = np.array([[-1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [1]])
-
 def KNN_test(X_train, Y_train, X_test, Y_test,K):
     NumberOfCorrect = 0
     radius = []
@@ -18,11 +12,14 @@ def KNN_test(X_train, Y_train, X_test, Y_test,K):
             NumberOfCorrect = NumberOfCorrect+1
     return NumberOfCorrect / len(X_test)
 
-# not going to lie I just forgot about the choose_K I'll finish that later
-# def choose_K(X_train,Y_train,X_val,Y_val):
-
-def main():
-    print(KNN_test(X,Y,XT, YT, 1))
-
-if __name__ == "__main__":
-    main()
+def choose_K(X_train,Y_train,X_val,Y_val):
+    OldAccuracy = 0
+    NewAccuracy = 0
+    Predicted_K = 1
+    K = len(X_train)
+    for i in range(0, K):
+        OldAccuracy = KNN_test(X_train,Y_train,X_val,Y_val, i)
+        if(OldAccuracy>NewAccuracy):
+            NewAccuracy = OldAccuracy
+            Predicted_K = i
+    return Predicted_K
