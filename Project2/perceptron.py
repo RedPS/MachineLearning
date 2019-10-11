735c0448e4bd398777dfc13d8bec4522ad7659fe
Pedram <psafaei@nevada.unr.edu>
2019-10-11 14:13:48 -0700

    I think this is good, let's test it more later

--- Project2/perceptron.py
+++ Project2/perceptron.py
@@ -1,19 +1,33 @@
 import numpy as np
 
-
 def perceptron_train(X,Y):
-    # initialize W and B (to 0)
-    # strat calculating a
-        # if a <=0 UPDATE
-        # else don't  
-        # set a flag for epoch so you know when to stop 
-        # so if W hasn't changed for that epoch we have converged and we're done
+    Done = False # converged or not 
+    epoch = 0 #keep track of epoch in case the data is not linearly separable
+    maxEpoch = 30
+    activation = 0 # initial activation
+    W_B_List = [[],[]] # 0 is weight, 1 is bias 
+    W_B_List[0] =[0]*len(X[0]) #initialize to 0
+    W_B_List[1] = [0]
+    while ( not Done or epoch < maxEpoch): # change the number of epochs to ur desire 
+        epoch+=1
+        for i in range (len(X)):
+            activation = 0
+            for m in range(len(X[i])):
+                activation += X[i][m] * W_B_List[0][m]
+            activation+= W_B_List[1][0]
+            if  (( activation * Y[i][0] ) < 1):
+                for j in range(len(X[0])):
+                    W_B_List[0][j] = W_B_List[0][j] + Y[i][0] * X[i][j]
+                W_B_List[1][0] = W_B_List[1][0] + Y[i][0]
+            else:
+                Done = True # if it is not going to update anymore change the value and get the hell out
+    return W_B_List
+
 def perceptron_test(X_test, Y_test, w, b):
-    # compare prediction with the label 
-    """
-    for i in X
-        calculate a 
-        if a*Y > 0
-        correct++
-    return the average correct
-    """
+    NumberOfCorrect = 0
+    activation = 0
+    for i, j in enumerate(X_test):
+        activation = np.dot(j,w) + b # could've done the above using the built in function, found out late!
+        if activation * Y_test[i] > 0:
+            NumberOfCorrect+=1
+    return NumberOfCorrect/len(X_test)
