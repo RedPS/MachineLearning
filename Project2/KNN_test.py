cc28468233c9f73fee3e771d284ed119e6359136
Pedram <psafaei@nevada.unr.edu>
2019-10-17 23:10:07 -0700

    made changes to get results for the report

--- Project2/KNN_test.py
+++ Project2/KNN_test.py
@@ -4,7 +4,7 @@ import numpy as np
 print_stuff = True
 
 if print_stuff:
-    print("\n\n\nKNN tests:")
+    print("KNN tests:")
 NN_X_train = np.array(
     [[1, 5], [2, 6], [2, 7], [3, 7], [3, 8], [4, 8], [5, 1], [5, 9], [6, 2], [7, 2], [7, 3], [8, 3], [8, 4],
      [9, 5]])
@@ -19,18 +19,18 @@ if print_stuff:
     print("KNN choose k on self =", choose_k_self)
 
 choose_test = NN.choose_K(NN_X_train, NN_Y_train, NN_test_X, NN_test_Y)
+print()
 if print_stuff:
     print("choose_k on test ", choose_test)
 
-i = 0
-while i < len(NN_test_X):
-    test_test = NN.KNN_test(NN_X_train, NN_Y_train, NN_test_X, NN_test_Y, i + 1)
+i = 9
+test_test = NN.KNN_test(NN_X_train, NN_Y_train, NN_test_X, NN_test_Y, i)
+if print_stuff:
+    print("KNN k=", i, "test on test accuracy =", test_test)
+print()
+for k in [9]:
     if print_stuff:
-        print("KNN k=", i + 1, "test on test accuracy =", test_test)
-    i += 1
-
-for k in [1, 2, 5]:
-    if print_stuff:
+        print()
         print("TESTING K =", k)
     for i in range(len(NN_test_X)):
         NN_res = NN.KNN_test(NN_X_train, NN_Y_train, np.array([NN_test_X[i]]), np.array([NN_test_Y[i]]), k)
