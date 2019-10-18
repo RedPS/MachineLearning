33b5319574ea087e1690bcfe8bd395bc7d97ec7c
Kevin-Carlos <kevin_carlos@nevada.unr.edu>
2019-10-18 16:18:21 -0700

    .

--- Project2/clustering.py
+++ Project2/clustering.py
@@ -1,11 +1,3 @@
-# 8: for k = 1 to K do
-# 9: Xk ← { xn : zn = k } // points assigned to cluster k
-# 10: µk ← mean(Xk
-# ) // re-estimate center of cluster k
-# 11: end for
-# 12: until µs stop changing
-# 13: return z // return cluster assignments
-
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -13,26 +5,27 @@ import random
 import time
 
 # Test
-# X = np.array([[1, 0], [7, 4], [9, 6], [2, 1], [4, 8], [0, 3], [13, 5], [6, 8], [7, 3], [3, 6], [2, 1], [8, 3], [10, 2], [3, 5], [5, 1], [1, 9], [10, 3], [4, 1], [6, 6], [2, 2]])
-X = np.array([[0], [1], [2], [7], [8], [9], [12], [14], [15]])
+X = np.array([[1, 0], [7, 4], [9, 6], [2, 1], [4, 8], [0, 3], [13, 5], [6, 8], [7, 3], [3, 6], [2, 1], [8, 3], [10, 2], [3, 5], [5, 1], [1, 9], [10, 3], [4, 1], [6, 6], [2, 2]])
+# X = np.array([[0], [1], [2], [7], [8], [9], [12], [14], [15]])
 
 def K_Means(X, K):
 
     PrePlotClusters(X, "before")
-    
+
     # Randomly initialize center
     randCenterIndexes = random.sample(range(len(X)), K)
     randCenters = []
     for k in randCenterIndexes:
         randCenters.append(X[k])
-    
+
     FindClusters(X, randCenters)
 
 def FindClusters(X, centers):
     oldCenters = []
+    error = 100
 
-    while oldCenters != centers:
-        print("going")
+
+    while error != 0:
         clusters = [[] for i in range(len(centers))]
         for i in range(len(X)):
             temp = 5444654
@@ -43,28 +36,46 @@ def FindClusters(X, centers):
                     temp = dist
                     value = X[i]
                     found = centerIndex
-            
+
+            # Store value in corresponding cluster (i.e found)
             clusters[found].append(value)
 
-        # Update cluster center by the mean
+        # Test if centers are the same, otherwise calculate new centers
+        print("C:", centers)
+        print("O:", oldCenters)
+        try:
+            if (np.allclose([centers], [oldCenters])):
+                error = 0
+        except:
+            pass
         oldCenters = centers.copy()
-        centers.clear()
-        
-        for i in range(len(oldCenters)):
-            number = np.mean(clusters[i])
-            centers.append(np.array([number]))
+
+        # Update cluster center by the mean
+        # for i in range(len(oldCenters)):
+        #     number = np.mean(clusters[i], axis=j)
+        #     print("Mean:", number)
+        #     centers.append(np.array([number])
+        # j = 0
+        # tempCent = []
+        # for i in range(len(oldCenters)):
+
+
+
 
     for i in centers:
         for j in i:
             print("Center:", j)
-        
+
     PlotClusters(clusters, "after")
 
 
 def calcDistance(point, centerPoint):
-
+    # print(point)
+    # print(centerPoint)
     distance = 0
     for i in range(point.shape[0]):
+        # print("CenterPt:", centerPoint[i])
+        # print("Pt:", point[i])
         value = (centerPoint[i] - point[i])**2
         distance += value
 
@@ -77,7 +88,6 @@ def PrePlotClusters(X, file):
     x = []
     y = []
 
-    # make this work for a x,y plot
     for index in range(len(X)):
         try:
             x.append(X[index][0])
@@ -90,34 +100,33 @@ def PrePlotClusters(X, file):
     except:
         plt.scatter(x, np.zeros_like(x))
 
-    plt.savefig(file + ".jpg")     
+    plt.savefig(file + ".png")
 
 def PlotClusters(clusters, file):
 
     x = []
-    colorString = 'red'
+    colorString = ['red', 'blue', 'yellow', 'green']
 
     for i in range(len(clusters)):
         for j in clusters[i]:
             for items in j:
                 print(items)
                 x.append(items)
-            
-        plt.scatter(x, np.zeros_like(x), c=colorString)
+
+        plt.scatter(x, np.zeros_like(x), c=colorString[i])
         x.clear()
-        colorString = 'blue'
         print("----------")
 
-    
-    plt.savefig(file + ".jpg")
+
+    plt.savefig(file + ".png")
 
 
-K_Means(X, 2)
+K_Means(X, 3)
 
 
 
 
-# I was just thinking about K_Means_better and tried to write down what was in my mind, the below is not code it is just some initial thoughts 
+# I was just thinking about K_Means_better and tried to write down what was in my mind, the below is not code it is just some initial thoughts
 """
 def K_Means_better(X, K):
   centers = []
