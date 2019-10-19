d4ffbdde266191652a6c9191301d58fa500d56a5
Kevin-Carlos <kevin_carlos@nevada.unr.edu>
2019-10-18 18:11:09 -0700

    Commented code and updated test script

--- Project2/Test.py
+++ Project2/Test.py
@@ -1,5 +1,6 @@
 from nearest_neighbors import *
 from perceptron import *
+from clustering import *
 
 # Data for KNN
 
@@ -9,7 +10,11 @@ YT = np.array([[-1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [-1]
 X = np.array ([[1, 1], [2, 1], [0, 10], [10, 10], [5, 5], [3, 10], [9, 4], [6, 2], [2, 2], [8, 7]])
 Y = np.array( [[1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [-1]])
 
-# Data for Perceptron 
+# Data for KMeans
+# KMeansX = np.array([[1, 0], [7, 4], [9, 6], [2, 1], [4, 8], [0, 3], [13, 5], [6, 8], [7, 3], [3, 6], [2, 1], [8, 3], [10, 2], [3, 5], [5, 1], [1, 9], [10, 3], [4, 1], [6, 6], [2, 2]])
+KMeansX = np.array([[0], [1], [2], [7], [8], [9], [12], [14], [15]])
+
+# Data for Perceptron
 Perceptron_X1 = np.array([[0, 1], [1, 0], [5, 4], [1, 1], [3, 3], [2, 4], [1, 6]])
 Perceptron_Y1 = np.array([[1], [1], [0], [1], [0], [0], [0]])
 Perceptron_X2 = np.array([[-2, 1], [1, 1], [1.5, -0.5], [-2, -1], [-1, -1.5], [2, -2]])
@@ -23,13 +28,15 @@ def main():
     print()
 
     print("-------------------- Part 2 ----------------------------\n")
+    print("KMeans:", K_Means(KMeansX, 3))
     print()
 
     print("-------------------- Part 3 ----------------------------\n")
-    print("Perceptron_Train:",perceptron_train(Perceptron_X2 , Perceptron_Y2))  # for test save these into w 
+    print("Perceptron_Train:",perceptron_train(Perceptron_X2 , Perceptron_Y2))  # for test save these into w
     W = perceptron_train(Perceptron_X2 , Perceptron_Y2)
     print("Perceptron_Test:",perceptron_test(Perceptron_X2, Perceptron_Y2, W[0], W[1]))
     print()
-    
+
+
 if __name__ == "__main__":
     main()
--- Project2/clustering.py
+++ Project2/clustering.py
@@ -2,11 +2,6 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import random
-import time
-
-# Test
-# X = np.array([[1, 0], [7, 4], [9, 6], [2, 1], [4, 8], [0, 3], [13, 5], [6, 8], [7, 3], [3, 6], [2, 1], [8, 3], [10, 2], [3, 5], [5, 1], [1, 9], [10, 3], [4, 1], [6, 6], [2, 2]])
-X = np.array([[0], [1], [2], [7], [8], [9], [12], [14], [15]])
 
 def K_Means(X, K):
 
@@ -16,7 +11,9 @@ def K_Means(X, K):
     for k in randCenterIndexes:
         randCenters.append(X[k])
 
-    FindClusters(X, randCenters)
+    output = FindClusters(X, randCenters)
+
+    return output
 
 def FindClusters(X, centers):
     oldCenters = []
@@ -43,23 +40,32 @@ def FindClusters(X, centers):
         # print("O:", oldCenters)
         try:
             if (np.allclose([centers], [oldCenters])):
-                print("Clusters:", clusters)
-                print("Centers:", centers)
+                # print("Clusters:", clusters)
+                # print("Centers:", centers)
                 error = 0
         except:
             pass
         oldCenters = centers.copy()
 
+        # Jank but to dynamically make the dimensions, e.g: x -- x,y -- x,y,z etc.
+        # And to store the new centers again
         dimensions = [[] for i in range(clusters[0][0].shape[0])]
         centers = [[] for i in range(len(centers))]
+
+        # Iterate over the cluster and store the values in their respective array
+        # depending on the dimensions noted above
         for i in range(len(centers)):
             for j in clusters[i]:
                 for val in range(len(j)):
                     dimensions[val].append(j[val])
 
+            # Iterate over the dimension list and calculate the mean for that cluster
+            # To get the new cluster center
             for numDim in range(len(dimensions)):
                 value = np.mean(dimensions[numDim])
                 centers[i].append(value)
+
+    return centers
 
 
 def calcDistance(point, centerPoint):
@@ -71,9 +77,6 @@ def calcDistance(point, centerPoint):
     distance = math.sqrt(distance)
 
     return distance
-
-K_Means(X, 2)
-
 
 
 
