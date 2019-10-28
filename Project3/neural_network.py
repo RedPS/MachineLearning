a74450efaeec914b7769984b4571b442e5899627
Pedram <psafaei@nevada.unr.edu>
2019-10-28 02:01:37 -0700

    more comment, needs more optimization

--- Project3/neural_network.py
+++ Project3/neural_network.py
@@ -37,6 +37,20 @@ of our build model.
 """
 # The below algorithm was actually taken from 
 # https://stackoverflow.com/questions/37292872/how-can-i-one-hot-encode-in-python
+"""
+You can think of this as having 
+[Coffee, Tea, Tea, Coffee]
+if we do integer encoding we can represent it as 
+[1, 0, 0, 1]
+now with one hot encoding we get 
+[ [0,1],
+  [1,0],
+  [1,0],
+  [0,1] ]
+
+The basic idea is that  we need a mechanism to somehow revert the output to the same format 
+as that of the input data. This is why we are using One-Hot Encoding
+"""
 def _one_hot_values(labels_data):
     encoded = [0] * len(labels_data)
     for j, i in enumerate(labels_data):
@@ -79,19 +93,27 @@ def build_model(X, y, nn_hdim, num_passes=20000, print_loss=True):
             print("iteration:", index, "Loss:", calculate_loss(model, X, y))
     return model
 
+"""
+The below is just what was given to us in the project description
+"""
 def plot_decision_boundary(pred_func, X, y):
+    # set min and max values and give it some padding
     x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
     y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
     h = 0.01
+    # Generate a grid of points with distance h between them
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
     Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
-
+# Generate a dataset and plot it 
 np.random.seed(0)
 X, y = make_moons(200, noise=0.20)
 plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
+# Generate ouputs with the following code 
 plt.figure(figsize=(16, 32))
 hidden_layer_dimensions = [1, 2, 3, 4]
 for i, nn_hdim in enumerate(hidden_layer_dimensions):
