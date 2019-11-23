10a63db4bc072add65064d8126080afc832a5c15
Pedram <psafaei@nevada.unr.edu>
2019-11-22 20:18:31 -0800

    deleted unecessary code

--- Project4/pca.py
+++ Project4/pca.py
@@ -8,11 +8,10 @@ def find_pcs(COV):
     L, PCS = np.linalg.eig(COV)
     return np.flip(L[np.argsort(L)], axis=0), (np.flip(PCS[np.argsort(L)], axis=0)).transpose()
 def project_data(Z,PCS,L,k,var):
-    if k > Z[0].size:
-        return np.dot(Z, PCS)
     if var > 0:
         k = np.max(np.argwhere(var >= (L.cumsum()/L.sum()))) + 1
-    return np.dot(Z, PCS[:, :k])
+        PCS = PCS[:, :k]
+    return np.dot(Z, PCS)
 """
 K = 0 was tricky, hopefully there was a function in numpy for calculating the cumalative since the assignment is not about 
 implemeting a cumalative function I just used the numpy one but here is the pseduo code 
